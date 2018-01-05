6cb150a7678797d72611ac0d044539a754d0c50a
Danilo Aleixo <danilo_aleixo@hotmail.com>
2018-01-04 23:22:51 -0200

    Fazendo o calendário
    - Preciso de uma função para colocar os offsets nas datas antes e depois do mês atual
    - Coloca mixins para margin e padding somente na lateral
    - Tive que decidir colocar o height no calendario fixo, entao o calendario tem 80% e o header tem 20%

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -12,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+	public today: Date;
+	public daysOfTheWeek: string[];
+	public month: Array<ICalendarDay[]>; 
 
-  ngOnInit() {
-  }
+  	constructor() {
+  		this.today = new Date(2017, 11, 18); // Dia padrão requisitado
+  		this.daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  		this.month = this.mountMonthInformation(this.today);
+  	}
 
+  	ngOnInit() {
+  	}
+
+
+  	// Move to service
+  	private getDaysInMonth(month: number, year: number): Date[] {
+        const date = new Date(year, month, 1);
+        let days = [];
+        while (date.getMonth() === month) {
+            days.push(new Date(date));
+            date.setDate(date.getDate() + 1);
+        }
+        return days;
+ 	}
+ 	private mountMonthInformation(today: Date): Array<ICalendarDay[]> {
+ 		let result: Array<ICalendarDay[]> = [];
+ 		const month: ICalendarDay[] = this.getDaysInMonth(today.getMonth(), today.getFullYear())
+ 		.map((day: Date) => {
+ 			return {
+ 				date: day,
+ 				offset: false
+ 			};
+ 		});
+
+ 		// Divide the days in arrays of 7 so instead of a array(31) I would have 
+ 		// [ array(7), array (7), ...]
+ 		for(let i = 0; i < month.length; i+= 7) {
+ 			result.push([]);
+ 			for(let j = 0; j < 7; j++) {
+ 				if(i + j < month.length)
+ 					result[result.length - 1].push(month[i + j])
+ 			}
+ 		}
+
+ 		return result;
+ 	}
+
+}
+
+interface ICalendarDay {
+	date: Date;
+	offset: boolean;
 }
